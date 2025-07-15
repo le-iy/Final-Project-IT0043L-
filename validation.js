3badd977397d5bf5c1feaf532f802c5909430f0b
Le.i <202410320@fit.edu.ph>
2025-07-15 14:00:26 +0800

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,15 @@
+// login validatino
+
+function loginValidation() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "newChick" && password === "24310") {
+        sessionStorage.setItem("loggedIn", "true");
+        window.location.href = "homePage.html"; // rekta sa home
+        return false;
+    } else {
+        alert("Wrong Egg. Please crack another.");
+        return false;
+    }
+}
